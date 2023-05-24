26b2480640005a4e17e402054f6469d2e7b3a8cb
Fatlir Morina <fatlirmorina666@gmail.com>
2023-05-24 10:11:08 +0200

    initialization

---
+++ first.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const pi float64 = 3.14
+
+	var Name string = "fatlir"
+
+	var mynumber int = 1
+
+	fmt.Println(mynumber, Name)
+
+	fmt.Println("starter app")
+}
